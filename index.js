ab72a6c5e5a31cf9f33b615d7a64640ef3d91a4e
Md Shahazad Uddin <shanudjn@gmail.com>
2021-06-13 09:57:47 +0530

    setup : adding index and added required dependencies

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const { initializeDBConnection } = require('./db/db.connect');
+
+
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
+
+
+initializeDBConnection();
+
+app.get('/', (req, res) => {
+    res.status(200).json({ "success": true, "message": "Hello Express App" })
+})
+app.listen(process.env.PORT || 8080, () => {
+    console.log('Server Started ✅')
+})
